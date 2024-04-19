ce3aabba44193709e735f04c3e5d41642b7d82b4
Wambui <nderituwambui93@gmail.com>
2024-04-19 23:37:00 +0300

    updates on canvas

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+// Add your code here
+
+const body = document.querySelector("body");
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((user) => {
+      body.innerHTML = user.id;
+    })
+    .catch((err) => {
+      body.innerHTML = err.message;
+    });
+}
